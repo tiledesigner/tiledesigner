4f783217001b9f5352ae869a49dc3526067b87b5
tiledesigner <33632486+tiledesigner@users.noreply.github.com>
2017-11-19 23:20:52 +0000

    color fix

--- index.js
+++ index.js
@@ -82,6 +82,175 @@ document.onkeydown = function(e){
       canvas.Redo();
     }
   };
+/*** CSS Color List ***/
+CSScolours=[["aliceblue","#F0F8FF"],
+["antiquewhite","#FAEBD7"],
+["aqua","#00FFFF"],
+["aqua","#00FFFF"],
+["aquamarine","#7FFFD4"],
+["azure","#F0FFFF"],
+["beige","#F5F5DC"],
+["bisque","#FFE4C4"],
+["black","#000000"],
+["black","#000000"],
+["blanchedalmond","#FFEBCD"],
+["blue","#0000FF"],
+["blue","#0000FF"],
+["blueviolet","#8A2BE2"],
+["brown","#A52A2A"],
+["burlywood","#DEB887"],
+["cadetblue","#5F9EA0"],
+["chartreuse","#7FFF00"],
+["chocolate","#D2691E"],
+["coral","#FF7F50"],
+["cornflowerblue","#6495ED"],
+["cornsilk","#FFF8DC"],
+["crimson","#DC143C"],
+["cyan","#00FFFF"],
+["darkblue","#00008B"],
+["darkbrown","#654321"],
+["darkcyan","#008B8B"],
+["darkgoldenrod","#B8860B"],
+["darkgray","#A9A9A9"],
+["darkgrey","#A9A9A9"],
+["darkgreen","#006400"],
+["darkkhaki","#BDB76B"],
+["darkmagenta","#8B008B"],
+["darkolivegreen","#556B2F"],
+["darkorange","#FF8C00"],
+["darkorchid","#9932CC"],
+["darkred","#8B0000"],
+["darksalmon","#E9967A"],
+["darkseagreen","#8FBC8F"],
+["darkslateblue","#483D8B"],
+["darkslategray","#2F4F4F"],
+["darkturquoise","#00CED1"],
+["darkviolet","#9400D3"],
+["deeppink","#FF1493"],
+["deepskyblue","#00BFFF"],
+["dimgray","#696969"],
+["dodgerblue","#1E90FF"],
+["firebrick","#B22222"],
+["floralwhite","#FFFAF0"],
+["forestgreen","#228B22"],
+["fuchsia","#FF00FF"],
+["fuchsia","#FF00FF"],
+["gainsboro","#DCDCDC"],
+["ghostwhite","#F8F8FF"],
+["gold","#FFD700"],
+["goldenrod","#DAA520"],
+["gray","#808080"],
+["grey","#7F7F7F"],
+["green","#008000"],
+["green","#008000"],
+["greenyellow","#ADFF2F"],
+["honeydew","#F0FFF0"],
+["hotpink","#FF69B4"],
+["indianred","#CD5C5C"],
+["indigo","#4B0082"],
+["ivory","#FFFFF0"],
+["khaki","#F0E68C"],
+["lavender","#E6E6FA"],
+["lavenderblush","#FFF0F5"],
+["lawngreen","#7CFC00"],
+["lemonchiffon","#FFFACD"],
+["lightblue","#ADD8E6"],
+["lightbrown","#B5651D"],
+["lightcoral","#F08080"],
+["lightcyan","#E0FFFF"],
+["lightgoldenrodyellow","#FAFAD2"],
+["lightgreen","#90EE90"],
+["lightgrey","#D3D3D3"],
+["lightgray","#D3D3D3"],
+["lightpink","#FFB6C1"],
+["lightsalmon","#FFA07A"],
+["lightseagreen","#20B2AA"],
+["lightskyblue","#87CEFA"],
+["lightslategray","#778899"],
+["lightsteelblue","#B0C4DE"],
+["lightyellow","#FFFFE0"],
+["lime","#00FF00"],
+["lime","#00FF00"],
+["limegreen","#32CD32"],
+["linen","#FAF0E6"],
+["magenta","#FF00FF"],
+["maroon","#800000"],
+["maroon","#800000"],
+["mediumaquamarine","#66CDAA"],
+["mediumblue","#0000CD"],
+["mediumorchid","#BA55D3"],
+["mediumpurple","#9370DB"],
+["mediumseagreen","#3CB371"],
+["mediumslateblue","#7B68EE"],
+["mediumspringgreen","#00FA9A"],
+["mediumturquoise","#48D1CC"],
+["mediumvioletred","#C71585"],
+["midnightblue","#191970"],
+["mintcream","#F5FFFA"],
+["mistyrose","#FFE4E1"],
+["moccasin","#FFE4B5"],
+["navajowhite","#FFDEAD"],
+["navy","#000080"],
+["navy","#000080"],
+["navyblue","#9FAFDF"],
+["oldlace","#FDF5E6"],
+["olive","#808000"],
+["olive","#808000"],
+["olivedrab","#6B8E23"],
+["orange","#FFA500"],
+["orange","#FFA500"],
+["orangered","#FF4500"],
+["orchid","#DA70D6"],
+["palegoldenrod","#EEE8AA"],
+["palegreen","#98FB98"],
+["paleturquoise","#AFEEEE"],
+["palevioletred","#DB7093"],
+["papayawhip","#FFEFD5"],
+["peachpuff","#FFDAB9"],
+["peru","#CD853F"],
+["pink","#FFC0CB"],
+["plum","#DDA0DD"],
+["powderblue","#B0E0E6"],
+["purple","#800080"],
+["purple","#800080"],
+["red","#FF0000"],
+["red","#FF0000"],
+["rosybrown","#BC8F8F"],
+["royalblue","#4169E1"],
+["saddlebrown","#8B4513"],
+["salmon","#FA8072"],
+["sandybrown","#FA8072"],
+["seagreen","#2E8B57"],
+["seashell","#FFF5EE"],
+["sienna","#A0522D"],
+["silver","#C0C0C0"],
+["silver","#C0C0C0"],
+["skyblue","#87CEEB"],
+["slateblue","#6A5ACD"],
+["slategray","#708090"],
+["snow","#FFFAFA"],
+["springgreen","#00FF7F"],
+["steelblue","#4682B4"],
+["tan","#D2B48C"],
+["teal","#008080"],
+["teal","#008080"],
+["thistle","#D8BFD8"],
+["tomato","#FF6347"],
+["turquoise","#40E0D0"],
+["violet","#EE82EE"],
+["wheat","#F5DEB3"],
+["white","#FFFFFF"],
+["white","#FFFFFF"],
+["whitesmoke","#F5F5F5"],
+["yellow","#FFFF00"],
+["yellow","#FFFF00"],
+["yellowgreen","#9ACD32"]];
+
+CSScoloursR=CSScolours.map(function(l){var ll=ArrayCopy(l); return ll.reverse();});
+
+EncodeColors=function(code){ReplaceReLi(code,CSScolours)};
+DecodeColors=function(code){ReplaceReLi(code,CSScoloursR)};
+
 /*** Main***/
 function Canvas(id){
   var c = document.getElementById(id);
@@ -835,13 +1004,23 @@ function Canvas(id){
     };
 
     titletoken=/^((?:\w)+)\s/;
-    colorstoken=/^((?:(?:transparent)|(?:\#(?:[a-f]|[0-9]){6})|(?:\#(?:[a-f]|[0-9]){3}))(\s|$))+/i;
+    colorstoken=/^((?:(?:transparent|aliceblue|antiquewhite|aqua|aqua|aquamarine|azure|beige|bisque|black|black|blanchedalmond|blue|blue|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkbrown|darkcyan|darkgoldenrod|darkgray|darkgrey|darkgreen|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dodgerblue|firebrick|floralwhite|forestgreen|fuchsia|fuchsia|gainsboro|ghostwhite|gold|goldenrod|gray|grey|green|green|greenyellow|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightbrown|lightcoral|lightcyan|lightgoldenrodyellow|lightgreen|lightgrey|lightgray|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightsteelblue|lightyellow|lime|lime|limegreen|linen|magenta|maroon|maroon|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|navy|navy|navyblue|oldlace|olive|olive|olivedrab|orange|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|purple|purple|red|red|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|silver|silver|skyblue|slateblue|slategray|snow|springgreen|steelblue|tan|teal|teal|thistle|tomato|turquoise|violet|wheat|white|white|whitesmoke|yellow|yellow|yellowgreen|)|(?:\#(?:[a-f]|[0-9]){6})|(?:\#(?:[a-f]|[0-9]){3}))(\s|$))+/i;
+    abrevtoken=/^(.)\s/;
     blocktoken=/^((?:(?:(?:\.|\d){5})(\s|$)){5})/i;
 
     var NextTile=function(code){
-      var title=code.match(titletoken)[1];
+      var title=code.match(titletoken);
+      if(title===null){console.log(code),console.log("title")}
+      title=title[1]
       code=code.replace(titletoken,"");
-      var colors=code.match(colorstoken)[0].replace(/\s$/,"");
+      var colors=code.match(colorstoken);
+      if(colors===null){
+        var abrev=code.match(abrevtoken,"");
+        code=code.replace(abrevtoken,"");
+        colors=code.match(colorstoken);}
+      if(colors===null){
+        console.log(code);}
+      colors=colors[0].replace(/\s$/,"");
       code=code.replace(colorstoken,"");
       block=code.match(blocktoken);
       code=code.replace(blocktoken,"");
